525fabcd254bd35c33aadb8052393e8ee12a204c
Jonathan Byron Correa Lindoso <jonathan.byron2908@gmail.com>
2018-08-24 10:04:02 +0200

    Using strings

--- app/src/main/java/com/jbyron/vehicleapp/view/BaseActivity.kt
+++ app/src/main/java/com/jbyron/vehicleapp/view/BaseActivity.kt
@@ -2,16 +2,17 @@ package com.jbyron.vehicleapp.view
 
 import android.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import com.jbyron.vehicleapp.R
 import com.jbyron.vehicleapp.util.FragmentUtil
 import timber.log.Timber
 
 open class BaseActivity : AppCompatActivity() {
     override fun onBackPressed() {
         val alertDialog = AlertDialog.Builder(this)
-            .setTitle("Android Common")
-            .setMessage("Are you sure do you want to quit the app?")
-            .setPositiveButton("Yes") { _, _ -> finish() }
-            .setNegativeButton("No") { _, _ -> Timber.i("I changed my mind...") }
+            .setTitle(getString(R.string.app_name))
+            .setMessage(getString(R.string.leave_message))
+            .setPositiveButton(getString(R.string.leave_yes)) { _, _ -> finish() }
+            .setNegativeButton(getString(R.string.leave_no)) { _, _ -> Timber.i("I changed my mind...") }
             .setCancelable(false)
             .create()
 
